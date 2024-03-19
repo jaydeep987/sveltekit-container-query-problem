a2c4279da156feb25fa36ce5788f51ef777015e7
jay <jaydeep253a@gmail.com>
2024-03-19 17:42:27 +0900

    initiated

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="main">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+
+<style lang="css">
+  .main {
+    container-name: myContainer;
+    container-type: inline-size;
+  }
+  @container myContainer (min-width: 300px) {
+      h1 {
+        color:red;
+      }
+    }
+</style>
